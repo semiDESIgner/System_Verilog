678e0383fe68950a645861cf5585840872128849
semiDESIgner <114133975+semiDESIgner@users.noreply.github.com>
2023-04-02 13:41:54 +0530

    Create assert.sv

---
+++ assert.sv
@@ -0,0 +1,15 @@
+module tb();
+  bit a=1'b0;
+ 
+  initial begin
+    assert(a)
+   
+    else
+    $warning("The value of a is false");
+    
+    
+  $display("The value of a us %0b",a); 
+    
+    
+  end
+ endmodule
